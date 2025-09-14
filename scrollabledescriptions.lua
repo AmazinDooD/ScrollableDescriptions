1f760806363822f384be2f2a1eb761ca7922e3c1
AmazinDooD <153538539+AmazinDooD@users.noreply.github.com>
2025-09-14 16:43:00 +0100

    delete line breaks so that its exactly 100 lines :3

--- scrollabledescriptions.lua
+++ scrollabledescriptions.lua
@@ -1,5 +1,4 @@
 local scr = SMODS.current_mod
-
 local ahp = Card.align_h_popup
 function Card:align_h_popup()
     local ret = ahp(self)
@@ -8,7 +7,6 @@ function Card:align_h_popup()
     ret.offset.y = ret.offset.y + self.scr.y
     return ret
 end
-
 ---Move a card's hover popup by the amount given in dir. dir should be a table with an x key and a y key.
 ---@param card Card
 ---@param dir table
@@ -18,12 +16,10 @@ function move_popup(card, dir)
     card.scr.x = card.scr.x + dir.x
     card.scr.y = card.scr.y + dir.y
 end
-
 function G.FUNCS.scr_switch_move_distance(arg)
     scr.config.move_distance = arg.to_key
     SMODS.save_mod_config(scr)
 end
-
 function scr.config_tab()
     local config_nodes =
     {n=G.UIT.ROOT, config = {align = "cm", colour = G.C.L_BLACK, minw = 4, minh = 4}, nodes = {
