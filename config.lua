680896274a98ed8b49ed16a87abdca34057e2067
AmazinDooD <153538539+AmazinDooD@users.noreply.github.com>
2025-09-17 17:21:27 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,3 +1,8 @@
 return {
-    ["move_distance"] = 4
+    ["move_distance"] = 4,
+    ["up_keybind"] = "UP",
+    ["down_keybind"] = "DOWN",
+    ["left_keybind"] = "LEFT",
+    ["right_keybind"] = "RIGHT",
+    ["home_keybind"] = "HOME"
 }
